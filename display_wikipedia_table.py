19a28d89c20d8bd5984d980f2ecd04b00a30e287
MarcoF <34093622+MarcoFosci@users.noreply.github.com>
2017-12-22 16:16:47 +0100

    Create display_wikipedia_table.py

---
+++ display_wikipedia_table.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+from IPython.display import HTML, display
+
+WIKI_URL = 'https://en.wikipedia.org/wiki/List_of_ongoing_armed_conflicts'
+req = requests.get(WIKI_URL)
+soup = BeautifulSoup(req.text, 'html5lib')
+table_classes = {'class': ['sortable', 'plainrowheaders']}
+table = soup.find_all('table', table_classes)
+display(HTML(str(table[0])))
