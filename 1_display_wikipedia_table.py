7d1ec4a80acfe59358f891157c4befa496cb6017
MarcoF <34093622+MarcoFosci@users.noreply.github.com>
2017-12-22 19:44:11 +0100

    Update 1_display_wikipedia_table.py

--- 1_display_wikipedia_table.py
+++ 1_display_wikipedia_table.py
@@ -5,6 +5,10 @@ from IPython.display import HTML, display
 WIKI_URL = 'https://en.wikipedia.org/wiki/List_of_ongoing_armed_conflicts'
 req = requests.get(WIKI_URL)
 soup = BeautifulSoup(req.text, 'html5lib')
-table_classes = {'class': ['sortable', 'plainrowheaders']}
+
+# Wikipedia use two CSS selectors for larger tables – sortable and plainrowheaders
+table_classes = {'class': ['sortable', 'plainrowheaders']} 
 table = soup.find_all('table', table_classes)
-display(HTML(str(table[0]))) # table[0] just to take the first table on the webpage 
+
+# I want to display the first table of the web page then I use table[0]
+display(HTML(str(table[0]))) 
